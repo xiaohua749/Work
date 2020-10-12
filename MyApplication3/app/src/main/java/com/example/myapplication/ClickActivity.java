bd1488bdf8aaefbfa259884917c41a4e9303fc8d
xiaohua749 <1159897725@qq.com>
2020-10-12 14:30:36 +0800

    first

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/ClickActivity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//public class ClickActivity extends AppCompatActivity {
+
+   // @Override
+    //protected void onCreate(Bundle savedInstanceState){
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_click);
+
+        //Button btn_click = findViewById(R.id.btn_click);
+
+        //btn_click.setOnClickListener(new MyOnClickListener());
+
+        //btn_click.setOnLongClickListener(new MyOnClickListener());
+
+    //}
+
+   // class MyOnClickListener implements View.OnClickListener{
+       // @Override
+        //public void onClick(View v){
+            //if(v.getId()==R.id.btn_click){
+                //Toast.makeText(context:ClickActivity.this,text:"您点击了控件："+((TextView) v).getText(),Toast.LENGTH_SHORT).show();
+
+           // }
+        //}
+    //}
+
+   // class MyOnLongClickListener implements View.OnLongClickListener{
+        //@Override
+        //public boolean onLongClick(View v){
+            //if(v.getId()==R.id.btn_click){
+               // Toast.makeText(context: ClickActivity.this,text:"您长按了控件："+((TextView) v).getText(),Toast.LENGTH_SHORT).show();
+
+           // }
+           // return true;
+        //}
+   // }
+//}
